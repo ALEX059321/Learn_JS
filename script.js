7d1f8b694b668c46de808d60246a9d206fba6cdb
ALEX059321 <raj05932134@gmail.com>
2026-06-07 22:38:38 +0530

    started loops

--- script.js
+++ script.js
@@ -30,14 +30,14 @@
 
 //if else 
 
-    const age = 18;
+    // const age = 18;
 
-    if(age >= 18){
-        console.log("you are eligible to vote"  );
-    }
-    else{
-        console.log("you are not eligible to vote");
-    }
+    // if(age >= 18){
+    //     console.log("you are eligible to vote"  );
+    // }
+    // else{
+    //     console.log("you are not eligible to vote");
+    // }
 
 
 
@@ -47,49 +47,82 @@
     // else if = contains more if else value in a nested way 
 
 
-    if(age >= 18){
-        console.log("you are an adult");
-    }
-    else if(age < 18){
-        console.log("you are a minor");
-    }
-    else if(age < 8){
-        console.log("you are a child")
-    }
-    else{
-        console.log("you are not born yet");
-    }
+    // if(age >= 18){
+    //     console.log("you are an adult");
+    // }
+    // else if(age < 18){
+    //     console.log("you are a minor");
+    // }
+    // else if(age < 8){
+    //     console.log("you are a child")
+    // }
+    // else{
+    //     console.log("you are not born yet");
+    // }
 
 
     // switch case
 
-    const day = 3;
+    // const day = 3;
 
-    switch(day){
-        case 1:
-            console.log("Monday")
-            break;
-        case 2:
-            console.log("tuesday")
-            break;
-        case 3:
-            console.log("Wednesday")
-            break;
-        case 4:
-            console.log("Thursday")
-            break;
-        case 5:
-            console.log("Friday")
-            break;
-        case 6:
-            console.log("Saturday")
-            break;
-        case 7:
-            console.log("Sunday")
-            break;
-        default:
-            console.log("Invalid day")
+    // switch(day){
+    //     case 1:
+    //         console.log("Monday")
+    //         break;
+    //     case 2:
+    //         console.log("tuesday")
+    //         break;
+    //     case 3:
+    //         console.log("Wednesday")
+    //         break;
+    //     case 4:
+    //         console.log("Thursday")
+    //         break;
+    //     case 5:
+    //         console.log("Friday")
+    //         break;
+    //     case 6:
+    //         console.log("Saturday")
+    //         break;
+    //     case 7:
+    //         console.log("Sunday")
+    //         break;
+    //     default:
+    //         console.log("Invalid day")
 
-    }
+    // }
 
-    OUTPUT: wednesday
+    // OUTPUT: wednesday
+
+
+    //LOOPS
+
+    //for loop 
+
+    // pattern 
+  /* 
+  for(start; end; change;){}
+            or
+  for(initilization; condition; increment/decrement){}
+  */
+
+//   for(let i = 1; i < 101; i++){
+//     console.log("hello world");
+//   }
+
+
+
+// while loop
+
+//pattern
+/*
+while(end){}
+     or 
+while(condition){}
+*/
+// let i = 1;
+// while(i < 101){
+
+//     console.log(i);
+//     i++;
+// }
